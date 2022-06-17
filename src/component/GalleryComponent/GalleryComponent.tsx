52399716b11f6520134c79b3d76ed5801e8c2b6a
daavid <davidvalen95>
2022-06-17 10:16:15 +0700

    search tag

--- src/component/GalleryComponent/GalleryComponent.tsx
+++ src/component/GalleryComponent/GalleryComponent.tsx
@@ -1,7 +1,6 @@
 import React, {FC, useEffect} from 'react';
 import styles from './GalleryComponent.module.scss';
 import axios from "axios";
-
 interface GalleryComponentProps {}
 
 
@@ -20,9 +19,9 @@ export class GalleryComponent extends  React.PureComponent{
 
         this.getData()
     }
-    public getData(){
+    getData(param={}){
 
-        axios.get('http://localhost:3015').then(axiosResponse=>{
+        axios.get('http://localhost:3015',{params:param}).then(axiosResponse=>{
             var result = axiosResponse.data
             console.log(result);
             this.setState({
@@ -31,6 +30,19 @@ export class GalleryComponent extends  React.PureComponent{
         })
     }
 
+  public  onSubmitSearchForm(e:any){
+        e.preventDefault();
+
+        let data = [...e.currentTarget.elements]
+            .filter((ele) => ele.type !== "submit")
+            .reduce((carry,current) => {
+
+                    carry[current.getAttribute("name")] = current.type === "file" ? current.files : current.value
+                return carry;
+            },{});
+        console.log(data);
+        this.getData(data)
+    }
 
     render() {
         return (
@@ -38,8 +50,18 @@ export class GalleryComponent extends  React.PureComponent{
 
                 <div className="container">
                     <div className="row">
-
+                        <div className="col-12 mb-5">
+                            <form onSubmit={(e)=>{this.onSubmitSearchForm(e)}}>
+                                <div className="form-group">
+                                    <label htmlFor="tag">Search by Tag</label>
+                                    <input name="tag" type="text" className="form-control" id="tag"
+                                           aria-describedby="emailHelp" placeholder="Search By Tag"/>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Search</button>
+                            </form>
+                        </div>
                         {
+
                             this.state.images.map((current)=>{
                                 return(
                                 <div className="col-6 col-md-4 ">
